0be3427f191723d1f9187b117b290397a7ecdd57
KBK <missing107@gmail.com>
2023-10-24 01:08:32 +0900

    Change Project
    Change Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,395 +1,111 @@
-import 'dart:async' as async;
-import 'dart:math';
-
-import 'package:dodge_side/helper/game_wall.dart';
-import 'package:flame/components.dart';
-import 'package:flame/effects.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/game.dart';
-import 'package:flame/parallax.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'component/bullet.dart';
+import 'models/settings.dart';
+import 'screens/main_menu.dart';
+import 'models/player_data.dart';
+import 'models/spaceship_details.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // This opens the app in fullscreen mode.
   await Flame.device.fullScreen();
-  await Flame.device.setPortrait();
-  var bulletHell = BulletHell();
-  runApp(GameWidget(game: bulletHell));
+
+  // Initialize hive.
+  await initHive();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        FutureProvider<PlayerData>(
+          create: (BuildContext context) => getPlayerData(),
+          initialData: PlayerData.fromMap(PlayerData.defaultData),
+        ),
+        FutureProvider<Settings>(
+          create: (BuildContext context) => getSettings(),
+          initialData: Settings(soundEffects: false, backgroundMusic: false),
+        ),
+      ],
+      builder: (context, child) {
+        // We use .value constructor here because the required objects
+        // are already created by upstream FutureProviders.
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider<PlayerData>.value(
+              value: Provider.of<PlayerData>(context),
+            ),
+            ChangeNotifierProvider<Settings>.value(
+              value: Provider.of<Settings>(context),
+            ),
+          ],
+          child: child,
+        );
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // Dark more because we are too cool for white theme.
+        themeMode: ThemeMode.dark,
+        // Use custom theme with 'BungeeInline' font.
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'BungeeInline',
+          scaffoldBackgroundColor: Colors.black,
+        ),
+        // MainMenu will be the first screen for now.
+        // But this might change in future if we decide
+        // to add a splash screen.
+        home: const MainMenu(),
+      ),
+    ),
+  );
 }
 
-late Vector2 viewPortSize;
+// This function initializes hive with app's
+// documents directory and also registers
+// all the hive adapters.
+Future<void> initHive() async {
+  await Hive.initFlutter();
 
-class BulletHell extends FlameGame with HasCollisionDetection {
-  @override
-  Future<void> onLoad() async {
-    super.onLoad();
-    viewPortSize = size;
-    camera.viewport = FixedResolutionViewport(size);
+  Hive.registerAdapter(PlayerDataAdapter());
+  Hive.registerAdapter(SpaceshipTypeAdapter());
+  Hive.registerAdapter(SettingsAdapter());
+}
 
-    await addParallaxBackground();
-    await addBoss();
-    await add(GameWall());
-  }
+/// This function reads the stored [PlayerData] from disk.
+Future<PlayerData> getPlayerData() async {
+  // Open the player data box and read player data from it.
+  final box = await Hive.openBox<PlayerData>(PlayerData.playerDataBox);
+  final playerData = box.get(PlayerData.playerDataKey);
 
-  Future<void> addBoss() async {
-    var imageSize = Vector2(101, 64);
-    final running = await loadSpriteAnimation(
-      'boss.png',
-      SpriteAnimationData.sequenced(
-        amount: 4,
-        textureSize: imageSize,
-        stepTime: 0.5,
-      ),
+  // If player data is null, it means this is a fresh launch
+  // of the game. In such case, we first store the default
+  // player data in the player data box and then return the same.
+  if (playerData == null) {
+    box.put(
+      PlayerData.playerDataKey,
+      PlayerData.fromMap(PlayerData.defaultData),
     );
-
-    var boss = SpriteAnimationComponent(
-        priority: 1,
-        animation: running,
-        anchor: Anchor.center,
-        size: imageSize,
-        angle: pi,
-        position: Vector2(size.x / 2.0, -10),
-        scale: Vector2(0.5, 0.5));
-
-    boss.add(SequenceEffect(
-      [
-        MoveEffect.to(
-            Vector2(size.x / 2.0, 500),
-            EffectController(
-                duration: 1, infinite: false, curve: Curves.bounceIn)),
-        MoveEffect.to(
-            Vector2(size.x / 2.0, 300),
-            EffectController(
-                duration: 1, infinite: false, curve: Curves.easeInExpo))
-      ],
-    ));
-
-    var counter = 10;
-    const patterns = 10;
-    var i = 0;
-    async.Timer.periodic(const Duration(seconds: 5), (timer) {
-      switch (i) {
-        case 0:
-          hellOne(counter, boss);
-          break;
-        case 1:
-          hellTwo(counter, boss);
-          break;
-        case 2:
-          hellThree(counter, boss);
-          break;
-        case 3:
-          hellFour(counter, boss);
-          break;
-        case 4:
-          hellFive(counter, boss);
-          break;
-        case 5:
-          hellSix(counter, boss);
-          break;
-        case 6:
-          hellSeven(counter, boss);
-          break;
-        case 7:
-          hellEight(counter, boss);
-          break;
-        case 8:
-          hellNine(counter, boss);
-          break;
-        case 9:
-          hellTen(counter, boss);
-          break;
-      }
-      i = (i + 1) % patterns;
-    });
-
-    add(boss);
   }
 
-  void hellTen(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
+  return box.get(PlayerData.playerDataKey)!;
+}
 
-      var positionComponent =
-          PositionComponent(position: boss.position, size: Vector2(600, 600));
+/// This function reads the stored [Settings] from disk.
+Future<Settings> getSettings() async {
+  // Open the settings box and read settings from it.
+  final box = await Hive.openBox<Settings>(Settings.settingsBox);
+  final settings = box.get(Settings.settingsKey);
 
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, -(pi / 37 - pi) * i, speed: speed)
-          ..position = Vector2.zero()
-          ..scale = boss.scale;
-        positionComponent.add(bullet
-          ..add(SequenceEffect([
-            OpacityEffect.to(
-              0.2,
-              SineEffectController(period: 2),
-            ),
-            OpacityEffect.to(
-              1,
-              SineEffectController(period: 2),
-            ),
-          ])));
-      }
-      add(positionComponent);
-    });
+  // If settings is null, it means this is a fresh launch
+  // of the game. In such case, we first store the default
+  // settings in the settings box and then return the same.
+  if (settings == null) {
+    box.put(Settings.settingsKey,
+        Settings(soundEffects: true, backgroundMusic: true));
   }
 
-  void hellNine(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale;
-
-        bullet.add(MoveAlongPathEffect(
-            Path()
-              ..lineTo(0, 100)
-              ..lineTo(100, 0)
-              ..lineTo(0, -100)
-              ..lineTo(-100, 0)
-              ..lineTo(0, 100)
-              ..close(),
-            SineEffectController(period: 2)));
-        add(bullet);
-      }
-    });
-  }
-
-  void hellEight(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      var positionComponent =
-          PositionComponent(position: boss.position, size: Vector2(600, 600));
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = Vector2.zero()
-          ..scale = boss.scale;
-        positionComponent.add(bullet);
-      }
-      add(positionComponent
-        ..add(RotateEffect.to(2 * pi, SineEffectController(period: 10))));
-    });
-  }
-
-  void hellSeven(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale
-          ..add(SequenceEffect([
-            OpacityEffect.to(
-              0.2,
-              SineEffectController(period: 2),
-            ),
-            OpacityEffect.to(
-              1,
-              SineEffectController(period: 2),
-            ),
-          ]));
-
-        add(bullet);
-      }
-    });
-  }
-
-  void hellSix(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale
-          ..add(SequenceEffect([
-            MoveAlongPathEffect(
-                Path()
-                  ..lineTo(
-                      Random().nextBool() ? 1 : -1 * Random().nextDouble() * 60,
-                      Random().nextBool()
-                          ? 1
-                          : -1 * Random().nextDouble() * 60),
-                NoiseEffectController(duration: 3, frequency: 100))
-          ]));
-
-        add(bullet);
-      }
-    });
-  }
-
-  void hellFive(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale
-          ..add((SequenceEffect([
-            MoveToEffect(Vector2(100, 200),
-                NoiseEffectController(duration: 2, frequency: 2)),
-          ])));
-
-        add(bullet);
-      }
-    });
-  }
-
-  void hellFour(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale
-          ..add(SequenceEffect([
-            GlowEffect(10.0, SineEffectController(period: 2),
-                style: BlurStyle.outer),
-            MoveByEffect(Vector2(0, 10), ZigzagEffectController(period: 0.2)),
-            MoveByEffect(Vector2(0, -10), ZigzagEffectController(period: 0.2)),
-            MoveByEffect(Vector2(10, 0), ZigzagEffectController(period: 0.2)),
-            MoveByEffect(Vector2(-10, 0), ZigzagEffectController(period: 0.2))
-          ]));
-
-        add(bullet);
-      }
-    });
-  }
-
-  void hellThree(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale
-          ..add(SequenceEffect([
-            MoveToEffect(Vector2(100, 200), SineEffectController(period: 8)),
-            MoveToEffect(Vector2(-100, -200), SineEffectController(period: 8)),
-          ]));
-
-        add(bullet);
-      }
-    });
-  }
-
-  void hellTwo(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale;
-
-        add(bullet);
-      }
-    });
-  }
-
-  void hellOne(int counter, SpriteAnimationComponent boss) {
-    double speed = 100;
-    var i = 0;
-    async.Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (timer.tick == counter) {
-        counter++;
-        timer.cancel();
-      }
-      var radians = i * 2 * pi / 10;
-      i = (i + 1) % 37;
-      var path = Path()..lineTo(cos(radians) * 100, sin(radians) * 100);
-
-      for (int i = 0; i < 37; i++) {
-        var bullet = Bullet(boss, (pi / 37 - pi) * i, speed: speed)
-          ..position = boss.position
-          ..scale = boss.scale;
-
-        bullet.add(MoveAlongPathEffect(path, SineEffectController(period: 2)));
-        add(bullet);
-      }
-    });
-  }
-
-  Future<void> addParallaxBackground() async {
-    final layerInfo = {
-      'background_1.png': 6.0,
-      'background_2.png': 8.5,
-      'background_3.png': 12.0,
-      'background_4.png': 20.5,
-    };
-
-    final layers = layerInfo.entries.map(
-      (entry) => loadParallaxLayer(
-        ParallaxImageData(entry.key),
-        fill: LayerFill.width,
-        repeat: ImageRepeat.repeat,
-        velocityMultiplier: Vector2(entry.value, entry.value),
-      ),
-    );
-
-    final parallax = ParallaxComponent(
-      parallax: Parallax(
-        await Future.wait(layers),
-        baseVelocity: Vector2(10, 10),
-      ),
-    );
-
-    Random().nextBool() ? ImageRepeat.repeatX : ImageRepeat.repeatY;
-    async.Timer.periodic(const Duration(seconds: 5), (timer) {
-      parallax.parallax?.baseVelocity = Vector2(
-        Random().nextBool()
-            ? Random().nextInt(20).toDouble()
-            : -Random().nextInt(20).toDouble(),
-        Random().nextBool()
-            ? Random().nextInt(20).toDouble()
-            : -Random().nextInt(20).toDouble(),
-      );
-    });
-    add(parallax);
-  }
+  return box.get(Settings.settingsKey)!;
 }
